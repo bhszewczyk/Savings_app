10bc68895f1833af39eb67b39adb8c24bbcd38ea
Barbara Szewczyk <comamysza@gmail.com>
2022-02-15 22:18:36 +0100

    Adds I

--- savings-app/src/App.js
+++ savings-app/src/App.js
@@ -1,50 +1,55 @@
+import React, { useState } from 'react';
+
 import Savings from './components/Savings/Savings';
 import NewSaving from './components/NewSaving/NewSaving';
 
+const DUMMY_EXPANSES = [
+	{
+		id: 's1',
+		title: 'Coffee Shop',
+		amount: 39.99,
+		date: new Date(2018, 8, 12),
+	},
+	{
+		id: 's2',
+		title: 'Shopping Lidl',
+		amount: 232.59,
+		date: new Date(2019, 8, 24),
+	},
+	{
+		id: 's3',
+		title: 'Shopping Biedronka',
+		amount: 154.26,
+		date: new Date(2020, 9, 3),
+	},
+	{
+		id: 's4',
+		title: 'Allegro',
+		amount: 145.88,
+		date: new Date(2021, 9, 12),
+	},
+	{
+		id: 's5',
+		title: 'Kokonki',
+		amount: 215.42,
+		date: new Date(2021, 9, 21),
+	},
+];
 function App() {
-	const savings = [
-		{
-			id: 's1',
-			title: 'Coffee Shop',
-			amount: 39.99,
-			date: new Date(2021, 8, 12),
-		},
-		{
-			id: 's2',
-			title: 'Shopping Lidl',
-			amount: 232.59,
-			date: new Date(2021, 8, 24),
-		},
-		{
-			id: 's3',
-			title: 'Shopping Biedronka',
-			amount: 154.26,
-			date: new Date(2021, 9, 3),
-		},
-		{
-			id: 's4',
-			title: 'Allegro',
-			amount: 145.88,
-			date: new Date(2021, 9, 12),
-		},
-		{
-			id: 's5',
-			title: 'Kokonki',
-			amount: 215.42,
-			date: new Date(2021, 9, 21),
-		},
-	];
+	const [expenses, setExpenses] = useState(DUMMY_EXPANSES);
 
-	const addSavingHandler = (saving) => {
+	const addSavingHandler = (expense) => {
 		console.log('In App.js');
-		console.log(saving);
+		setExpenses((prevExpenses) => {
+			return [expense, ...prevExpenses];
+		});
 	};
 
 	return (
 		<div>
 			<h1>Track your savings</h1>
 			<NewSaving onAddSaving={addSavingHandler} />
-			<Savings savings={savings} />
+			<Savings savings={expenses} />
 		</div>
 	);
 }
