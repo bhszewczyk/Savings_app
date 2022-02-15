61a02ea86f99b5632499f2cf39a3a6d4737243db
Barbara Szewczyk <comamysza@gmail.com>
2022-02-15 21:19:44 +0100

    Adds SavingsFilter component (without updating UI)

--- savings-app/src/components/Savings/Savings.js
+++ savings-app/src/components/Savings/Savings.js
@@ -1,38 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './Savings.css';
 import SavingItem from './SavingItem';
 import Card from '../UI/Card';
+import SavingFilter from './SavingsFilter';
 
 function Savings(props) {
+	const [choosenYear, setEnteredYear] = useState('');
+
+	const filterHandler = (selectedYear) => {
+		console.log(selectedYear);
+		setEnteredYear(selectedYear);
+	};
+
 	return (
-		<Card className='savings'>
-			<SavingItem
-				title={props.savings[0].title}
-				amount={props.savings[0].amount}
-				date={props.savings[0].date}
-			/>
-			<SavingItem
-				title={props.savings[1].title}
-				amount={props.savings[1].amount}
-				date={props.savings[1].date}
-			/>
-			<SavingItem
-				title={props.savings[2].title}
-				amount={props.savings[2].amount}
-				date={props.savings[2].date}
-			/>
-			<SavingItem
-				title={props.savings[3].title}
-				amount={props.savings[3].amount}
-				date={props.savings[3].date}
-			/>
-			<SavingItem
-				title={props.savings[4].title}
-				amount={props.savings[4].amount}
-				date={props.savings[4].date}
-			/>
-		</Card>
+		<div>
+			<Card className='savings'>
+				<SavingFilter selection={choosenYear} onFilter={filterHandler} />
+				<SavingItem
+					title={props.savings[0].title}
+					amount={props.savings[0].amount}
+					date={props.savings[0].date}
+				/>
+				<SavingItem
+					title={props.savings[1].title}
+					amount={props.savings[1].amount}
+					date={props.savings[1].date}
+				/>
+				<SavingItem
+					title={props.savings[2].title}
+					amount={props.savings[2].amount}
+					date={props.savings[2].date}
+				/>
+				<SavingItem
+					title={props.savings[3].title}
+					amount={props.savings[3].amount}
+					date={props.savings[3].date}
+				/>
+				<SavingItem
+					title={props.savings[4].title}
+					amount={props.savings[4].amount}
+					date={props.savings[4].date}
+				/>
+			</Card>
+		</div>
 	);
 }
 
---
+++ savings-app/src/components/Savings/SavingsFilter.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import './SavingsFilter.css';
+
+const SavingsFilter = (props) => {
+	// const [choosenYear, setEnteredYear] = useState('');
+
+	const filterChangeHandler = (e) => {
+		// setEnteredYear(e.target.value);
+		props.onFilter(e.target.value);
+	};
+
+	return (
+		<div className='savings-filter'>
+			<div className='savings-filter__control'>
+				<label>Filter by year</label>
+				<select onChange={filterChangeHandler}>
+					<option value='2023'>2023</option>
+					<option value='2022'>2022</option>
+					<option value='2021'>2021</option>
+					<option value='2020'>2020</option>
+					<option value='2019'>2019</option>
+					<option value='2018'>2018</option>
+				</select>
+			</div>
+		</div>
+	);
+};
+
+export default SavingsFilter;
